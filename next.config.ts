1dffba4444f73b36f6ec6d98e0a71b954dea8cd6
Bapi Biswas <107844015+bapibiswas100808@users.noreply.github.com>
2026-04-01 18:00:00 +0600

    updated

--- next.config.ts
+++ next.config.ts
@@ -14,8 +14,8 @@ const nextConfig = {
         hostname: "res.cloudinary.com",
       },
       {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
+        protocol: "https",
+        hostname: "i.ibb.co",
       },
     ],
   },
