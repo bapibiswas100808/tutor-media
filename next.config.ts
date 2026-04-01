ee727d5ade72a0e73e8ab5f6993e664964f6e412
Bapi Biswas <107844015+bapibiswas100808@users.noreply.github.com>
2026-04-02 00:16:04 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true, // Disable sharp for Linux VPS compatibility
-    // remotePatterns: [
-    //   {
-    //     protocol: "http",
-    //     hostname: "localhost",
-    //     port: "1337",
-    //     pathname: "/uploads/**",
-    //   },
-    //   {
-    //     protocol: "https",
-    //     hostname: "res.cloudinary.com",
-    //   },
-    //   {
-    //     protocol: "https",
-    //     hostname: "i.ibb.co",
-    //   },
-    // ],
+    unoptimized: true,
   },
 };
 
