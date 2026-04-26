c7a6f569acd9a0bd256f15b1f11ebe243e3b9111
Bapi Biswas <107844015+bapibiswas100808@users.noreply.github.com>
2026-04-27 00:12:47 +0600

    updated

--- src/app/tutor-hub/TutorHubClient.tsx
+++ src/app/tutor-hub/TutorHubClient.tsx
@@ -59,7 +59,7 @@ export default function TutorHubPage({ tutorHubs }: { tutorHubs: Tutor[] }) {
           animate={{ opacity: 1, y: 0 }}
           className="text-center mb-6"
         >
-          <h1 className="text-3xl md:text-4xl font-bold mb-1 bg-gradient-to-r from-blue-900 to-purple-900 bg-clip-text text-transparent">
+          <h1 className="text-3xl md:text-4xl font-bold mb-1 bg-linear-to-r from-blue-900 to-purple-900 bg-clip-text text-transparent">
             Find Expert Mentors
           </h1>
           <p className="text-gray-600 text-lg max-w-2xl mx-auto">
